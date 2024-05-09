21ffe5b4340d322f4ffb5cac04c20e679a183cce
HeyHew <mike@heyhew.net>
2024-05-09 17:14:29 -0400

    Initial Scripts
    Initial Scripts

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright © 2024 Michael Kudrak <mike@heyhew.net>
+# This code was developed under an Apache License V2.0
+clear
+echo "This script needs to be run as a normal user with sudo permissions"
+sleep 1
+su - -c 'apt update && apt upgrade -y'
+curl -fsSL https://ollama.com/install.sh | sh
+echo "Getting llama3 8 Billion"
+sleep 1
+ollama pull llama3
+echo "Getting starcoder. 80+ progamming languages"
+ollama pull starcoder
+echo "Getting Wizard Coder latest"
+ollama pull wizardcoder
---
+++ script_cuda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright © 2024 Michael Kudrak <mike@heyhew.net>
+# This code was developed under an Apache License V2.0
+clear
+echo "This script needs to be run as a normal user with sudo permissions"
+sleep 1
+su - -c 'apt update && apt upgrade -y'
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+su - -c 'dpkg -i cuda keyring_1.1-1_all.deb'
+su - -c 'apt update && apt install -y cuda-toolkit-12-4'
+su - -c 'apt install -y cuda-drivers'
+curl -fsSL https://ollama.com/install.sh | sh
+echo "Getting llama3 8 Billion"
+sleep 1
+ollama pull llama3
+echo "Getting starcoder. 80+ progamming languages"
+ollama pull starcoder
+echo "Getting Wizard Coder latest"
+ollama pull wizardcoder
