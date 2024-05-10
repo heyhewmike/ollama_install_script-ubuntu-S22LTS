f0c99379f3db4e939286896eb8e46b26cca7341a
HeyHew <mike@heyhew.net>
2024-05-10 10:45:26 -0400

    minor cuda updates

--- script_cuda.sh
+++ script_cuda.sh
@@ -4,10 +4,10 @@
 clear
 echo "This script needs to be run as a normal user with sudo permissions"
 sleep 1
-cat "$nrconf{restart} = 'a';" > /etc/needrestart/conf.d/no-prompt.conf
+echo "$nrconf{restart} = 'a';" > /etc/needrestart/conf.d/no-prompt.conf
 apt update && apt upgrade -y
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-dpkg -i cuda keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
 apt update && apt install -y cuda-toolkit-12-4 cuda-drivers
 # apt install -y cuda-drivers
 curl -fsSL https://ollama.com/install.sh | sh
