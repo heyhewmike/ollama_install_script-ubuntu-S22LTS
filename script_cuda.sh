152749b20a20e0fb32c44fc50335e61510f45568
HeyHew <mike@heyhew.net>
2024-05-10 10:27:37 -0400

    clean up
    cleaning up

--- script_cuda.sh
+++ script_cuda.sh
@@ -4,14 +4,15 @@
 clear
 echo "This script needs to be run as a normal user with sudo permissions"
 sleep 1
+cat "$nrconf{restart} = 'a';" > /etc/needrestart/conf.d/no-prompt.conf
 apt update && apt upgrade -y
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda keyring_1.1-1_all.deb
-apt update && apt install -y cuda-toolkit-12-4
-apt install -y cuda-drivers
+apt update && apt install -y cuda-toolkit-12-4 cuda-drivers
+# apt install -y cuda-drivers
 curl -fsSL https://ollama.com/install.sh | sh
 echo "Getting llama3 8 Billion"
-sleep 1
+# sleep 1
 ollama pull llama3
 echo "Getting starcoder. 80+ progamming languages"
 ollama pull starcoder
